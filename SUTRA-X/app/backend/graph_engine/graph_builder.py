380dcc36ebad3b845515dc02f6e3caee63ee544c
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-09-06 23:13:36 +0530

    Update graph_builder.py

--- SUTRA-X/app/backend/graph_engine/graph_builder.py
+++ SUTRA-X/app/backend/graph_engine/graph_builder.py
@@ -2,32 +2,57 @@
 Graph Builder Utilities
 """
 
+import sys
+from pathlib import Path
+
+# Try to import networkx
+try:
+    import networkx as nx
+    NETWORKX_AVAILABLE = True
+except ImportError:
+    NETWORKX_AVAILABLE = False
+
 def get_node_list(G):
     try:
-        return list(G.nodes())
+        if NETWORKX_AVAILABLE:
+            return list(G.nodes())
+        else:
+            return list(G.nodes)
     except:
         return []
 
 def get_node_attributes(G, node):
     try:
-        return dict(G.nodes[node])
+        if NETWORKX_AVAILABLE:
+            return dict(G.nodes[node])
+        else:
+            return G.nodes[node]
     except:
         return {}
 
 def get_neighbors(G, node):
     try:
-        return list(G.neighbors(node))
+        if NETWORKX_AVAILABLE:
+            return list(G.neighbors(node))
+        else:
+            return G.neighbors(node)
     except:
         return []
 
 def get_degree(G, node):
     try:
-        return G.degree(node)
+        if NETWORKX_AVAILABLE:
+            return G.degree(node)
+        else:
+            return G.degree(node)
     except:
         return len(get_neighbors(G, node))
 
 def get_edge_data(G, u, v):
     try:
-        return G.get_edge_data(u, v)
+        if NETWORKX_AVAILABLE:
+            return G.get_edge_data(u, v)
+        else:
+            return G.get_edge_data(u, v)
     except:
         return {}
