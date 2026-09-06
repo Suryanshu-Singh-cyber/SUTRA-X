36eaea66dd5a8866947c164dab38f35c3dfdf568
Suryanshu Singh <thesuryanshu16@gmail.com>
2026-09-06 23:11:04 +0530

    Create init.py

---
+++ SUTRA-X/app/backend/init.py
@@ -0,0 +1,16 @@
+"""
+Backend Module
+"""
+
+from . import core
+from . import data_ingestion
+from . import graph_engine
+from . import intelligence_engine
+from . import nlp_engine
+from . import rag
+from . import heatmap
+from . import export
+from . import security
+from . import database
+from . import utils
+from . import data
